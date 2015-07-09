25c67df708b52889adf8a7fb7aa84a68844a2e4f
s1312022 <s1312022@PCLL-61.rmme.do-johodai.ac.jp>
2015-07-09 13:38:43 +0900

    4章前

---
+++ src/com/example/sudoku/Sudoku.java
@@ -0,0 +1,81 @@
+package com.example.sudoku;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.util.Log;
+
+
+public class Sudoku extends Activity implements OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		//すべてのボタンについてクリックリスナをセットアップする
+		View continueButton = findViewById(R.id.continue_button);
+		continueButton.setOnClickListener(this);
+		View newButton = findViewById(R.id.new_button);
+		newButton.setOnClickListener(this);
+		View aboutButton = findViewById(R.id.about_button);
+		aboutButton.setOnClickListener(this);
+		View exitButton = findViewById(R.id.exit_button);
+		exitButton.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.sudoku, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.about_button:
+			Intent i = new Intent(this, About.class);
+			startActivity(i);
+			break;
+		case R.id.new_button:
+			openNewGameDialog();
+			break;
+		//ここに別のボタンの処理コード（もしあれば）
+		}
+	}
+
+	private static final String TAG = "Sudoku";
+	private void openNewGameDialog() {
+		new AlertDialog.Builder(this)
+		.setTitle(R.string.new_game_title)
+		.setItems(R.array.difficulty,  new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialoginterface, int i) {
+				startGame(i);
+			}
+		})
+		.show();
+	}
+
+	private void startGame(int i) {
+		Log.d(TAG,"clicked on" +i);
+		//ここでゲームを開始する
+	}
+}
